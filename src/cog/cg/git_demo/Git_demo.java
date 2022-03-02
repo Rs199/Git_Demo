32bc9973537b59d5ec2059a754d1d1f64c2d5ac9
Rs199 <way2rishabh99@gmail.com>
2022-03-02 14:34:43 +0530

    New Update
    Version 2

--- src/cog/cg/git_demo/Git_demo.java
+++ src/cog/cg/git_demo/Git_demo.java
@@ -4,6 +4,7 @@ public class Git_demo {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Hello again");
+		System.err.println("Hello Yet Again");
 	}
 
 }
